c4661bff216c7c391b52879d05d9faccc011e068
Lory Hou <lory.hou@augmentum.com>
2017-04-06 13:27:53 +0800

    add v2

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,11 +1,18 @@
 package com.example;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
@@ -19,6 +26,32 @@ public class DemoApplication {
     model.put("content", "Hello World");
     return model;
   }
+
+   @RequestMapping("/user")
+  public Principal user(Principal user) {
+    return user;
+  }
+
+  @Configuration
+  @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+        .httpBasic()
+      .and()
+        .authorizeRequests()
+          .antMatchers("/index.html", "/home.html", "/login.html", "/")
+          .permitAll()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .csrf()
+          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+  }
+
+
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,7 +1,78 @@
-angular.module('hello', [])
-  .controller('home', function($http) {
-     var self = this;
-  	 $http.get('/resource/').then(function(response) {
-    	self.greeting = response.data;
+angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider, $httpProvider) {
+
+  $routeProvider.when('/', {
+    templateUrl : 'home.html',
+    controller : 'home',
+    controllerAs : 'controller'
+  }).when('/login',{
+    templateUrl : 'login.html',
+    controller : 'navigation',
+    controllerAs: 'controller'
+  }).otherwise('/');
+
+  $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+  $httpProvider.defaults.headers.common['Accept'] = 'application/json';
+
+}).controller('navigation',
+  function($rootScope, $http, $location, $route) {
+    var self = this;
+    self.tab = function(route) {
+      return $route.current && route === $route.current.controller;
+    };
+    $http.get('user').then(function(response) {
+      if (response.data.name) {
+        $rootScope.authenticated = true;
+      } else {
+        $rootScope.authenticated = false;
+      }
+    }, function() {
+      $rootScope.authenticated = false;
+    });
+
+var authenticate = function(credentials, callback) {
+
+    var headers = credentials ? {authorization : "Basic "
+        + btoa(credentials.username + ":" + credentials.password)
+    } : {};
+
+    $http.get('user', {headers : headers}).then(function(response) {
+      if (response.data.name) {
+        $rootScope.authenticated = true;
+      } else {
+        $rootScope.authenticated = false;
+      }
+      callback && callback();
+    }, function() {
+      $rootScope.authenticated = false;
+      callback && callback();
+    });
+
+  }
+
+  authenticate();
+  self.credentials = {};
+  self.login = function() {
+      authenticate(self.credentials, function() {
+        if ($rootScope.authenticated) {
+          $location.path("/");
+          self.error = false;
+        } else {
+          $location.path("/login");
+          self.error = true;
+        }
+      });
+  };
+
+  self.logout = function() {
+    $http.post('logout', {}).finally(function() {
+      $rootScope.authenticated = false;
+      $location.path("/");
+    });
+  }
+
+}).controller('home', function($http) {
+  var self = this;
+  $http.get('resource/').then(function(response) {
+    self.greeting = response.data;
   })
-})
+});
